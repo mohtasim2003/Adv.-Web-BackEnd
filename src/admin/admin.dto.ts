744fea165761d4b1fbd457541ccca41631a3968f
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2025-11-11 02:18:40 +0600

    small changes

--- src/admin/admin.dto.ts
+++ src/admin/admin.dto.ts
@@ -1,3 +1,12 @@
+//import all from 'class-validator';
+/*
+Name Must not contain any special character
+• Password field must be at least 6 character long and it must contain one
+Lowercase character
+• Validate the file uploaded is in PDF format
+• Phone Number field must start with 01*/
+
+
 export class AdminDTO {
     id: number;
     name: string;
